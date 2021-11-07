7e7ca72f6a9ae74e4aa19ef51dcc9bdbc177b5b3
shajil2020 <shajilgeorge2020@gmail.com>
2021-11-07 12:10:13 +0530

    Initial Deployment

---
+++ fingent_cart/application/models/products_model.php
@@ -0,0 +1,30 @@
+<?php if (!defined('BASEPATH')) exit('No direct script access allowed');
+
+class Products_model extends CI_Model {
+
+	public function __construct()
+	{
+		//$this->load->database();
+	}
+
+	public function get_all()
+	{
+		$query = $this->db->get('products');
+		return $query->result_array();
+	}
+	public function insert_products($products_list=null)
+	{
+		$this->db->insert('cart_products',$products_list);
+	}
+	public function get_cart_products()
+	{
+		$query = $this->db->get('cart_products');
+		return $query->result_array();
+	}
+	public function get_product_count($prodcut_id)
+	{
+       $this->db->where('product_id',$prodcut_id);
+       $query = $this->db->get('cart_products');
+       return $query->result_array();
+	}
+}
